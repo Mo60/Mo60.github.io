cb2b1c6702cbaf993ca59f4a514c9fd93c1461f9
Mo Khalil <mokhalil3@hotmail.com>
2023-07-31 00:51:17 -0500

    new build

--- docs/assets/home-77a10837.js
+++
@@ -1,3 +0,0 @@
-import { h as _, o as a, g as c, b as e, r as n, c as r, e as s } from './app-2d068019.js'
-
-const m = {}; const i = { 'px-4': '', 'py-10': '', 'text': 'center gray-700 dark:gray-200' }; const p = s('div', { 'mx-auto': '', 'mt-5': '', 'text-center': '', 'text-sm': '', 'opacity-50': '' }, ' [Home Layout] ', -1); function d(x, f) { const t = n('RouterView'); const o = _; return a(), r('main', i, [e(t), e(o), p]) } const h = c(m, [['render', d]]); export { h as default }
